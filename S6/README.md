ee66c6ee0957996273d21f1c1bc223a0d804fbf3
Praveen Behara <praveen.behara@yahoo.com>
2023-03-28 00:07:05 +0530

    updated code and readme

--- S6/README.md
+++ S6/README.md
@@ -1,1 +1,43 @@
 # Session 6 readme.md file
+
+## Assignment 6
+
+    Run this network 
+
+Links to an external site..
+Fix the network above:
+
+    change the code such that it uses GPU and
+    change the architecture to C1C2C3C40 (No MaxPooling, but 3 3x3 layers with stride of 2 instead) (If you can figure out how to use Dilated kernels here instead of MP or strided convolution, then 200pts extra!)
+    total RF must be more than 44
+    one of the layers must use Depthwise Separable Convolution
+    one of the layers must use Dilated Convolution
+    use GAP (compulsory):- add FC after GAP to target #of classes (optional)
+    use albumentation library and apply:
+        horizontal flip
+        shiftScaleRotate
+        coarseDropout (max_holes = 1, max_height=16px, max_width=1, min_holes = 1, min_height=16px, min_width=16px, fill_value=(mean of your dataset), mask_fill_value = None)
+    achieve 85% accuracy, as many epochs as you want. Total Params to be less than 200k.
+    upload to Github
+    Attempt S6-Assignment Solution.
+    Questions in the Assignment QnA are:
+        copy paste your model code from your model.py file (full code) [125]
+        copy paste output of torchsummary [125]
+        copy-paste the code where you implemented albumentation transformation for all three transformations [125]
+        copy paste your training log (you must be running validation/text after each Epoch [125]
+        Share the link for your README.md file. [200]
+
+## Solution
+The model.py has the code for the CNN as well as a new class for Depthwise Separable Convultion.
+The network is a 4 layer convolution followed by an output layer.
+The Depthwise Separable convolution layer is a 3x3 with padding 1, followed by a 1x1.
+It is used in both the transition block as well as the convolution block.
+A GAP layer is added at the end to flatten out the image.
+
+The S6_Assignment.ipynb imports the network in model.py and trains it on the CIFAR10 dataset.
+The mean and standard deviation of the CIFAR10 dataset is calculated for later use.
+The class for performing Albumentations and providing it as a dataset is created.
+It defines image augmentations such as graying, normalizations (based on mean / std above), flipping, shift scale, cut-out of training images and only normalization of the test images.
+
+The network is instantiated and run for 100 epochs. 
+At around epoch 66 we still that a steady 85% mark is breach and crosses and stays at 87% by epoch 100.
--- S6/model.py
+++ S6/model.py
@@ -1,1 +1,96 @@
+import torch.nn as nn
+import torch.nn.functional as F
 
+# Depthwise Convolution class
+class DepthwiseSeparable(nn.Module):
+    def __init__(self, in_ch, out_ch, stride=1):
+        super(DepthwiseSeparable, self).__init__()
+        self.in_ch = in_ch
+        self.out_ch = out_ch
+        self.stride = stride
+
+        self.depthwise = nn.Sequential(
+            nn.Conv2d(in_channels=self.in_ch, out_channels=self.in_ch, kernel_size=(3, 3), padding=1, stride=self.stride, groups=self.in_ch, bias=False),
+            nn.Conv2d(in_channels=self.in_ch, out_channels=self.out_ch, kernel_size=(1, 1), bias=False)
+        )
+
+    def forward(self, x):
+        return self.depthwise(x)
+    
+# The network
+class Net(nn.Module):
+    def __init__(self, drop=0.025):
+        super(Net, self).__init__()
+
+        # Define C1 C2 C3 C4 Output blocks
+        self.conblock1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=(3, 3), padding=1, bias=False), # I: 32x32x3 | O: 32x32x32 | RF: 3x3
+            nn.BatchNorm2d(num_features=32),
+            nn.ReLU(),
+            nn.Dropout(p=drop),
+
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), padding=1, bias=False), # I: 32x32x32 | O: 32x32x32 | RF: 5x5
+            nn.BatchNorm2d(num_features=32),
+            nn.ReLU(),
+            nn.Dropout(p=drop)
+        )
+
+        self.transblock1 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), padding=1, stride=2, bias=False), #I: 32x32x32 | O: 16x16x32 | RF: 7x7
+            nn.BatchNorm2d(num_features=32),
+            nn.ReLU(),
+            nn.Dropout(p=drop)
+        )
+        
+        self.conblock2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding=1, bias=False), # I: 16x16x32 | O: 16x16x64 | RF: 11x11
+            nn.BatchNorm2d(num_features=64),
+            nn.ReLU(),
+            nn.Dropout(p=drop)
+        )
+
+        self.transblock2 = nn.Sequential(
+            DepthwiseSeparable(in_ch=64, out_ch=64, stride=2), #I: 16x16x64 | O: 8x8x64 | RF: 15x15
+            nn.BatchNorm2d(num_features=64),
+            nn.ReLU(),
+            nn.Dropout(p=drop)
+        )
+
+        self.conblock3 = nn.Sequential(
+            DepthwiseSeparable(in_ch=64, out_ch=128), #I: 8x8x64 | O: 8x8x128 | RF: 23x23
+            nn.BatchNorm2d(num_features=128),
+            nn.ReLU(),
+            nn.Dropout(p=drop),
+
+            nn.Conv2d(in_channels=128, out_channels=32, kernel_size=(1, 1), bias=False), # I: 8x8x128 | O: 8x8x32 | RF: 23x23
+            nn.BatchNorm2d(num_features=32),
+            nn.ReLU(),
+            nn.Dropout(p=drop)
+        )
+
+        self.transblock3 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), padding=1, dilation=2, bias=False), # I: 8x8x32 | O: 6x6x32 | RF: 39x39
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), padding=1, dilation=2, bias=False), # I: 6x6x32 | O: 4x4x32 | RF: 55x55
+            nn.BatchNorm2d(num_features=32),
+            nn.ReLU(),
+            nn.Dropout(p=drop)
+        )
+
+        self.conblock4 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), padding=1, bias=False), # I: 4x4x32 | O: 4x4x32 | RF: 63x63
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=10, kernel_size=(3, 3), padding=1, bias=False), # I: 4x4x32 | O: 4x4x10 | RF: 71x71
+            
+        )
+
+        self.gap = nn.AvgPool2d(kernel_size=4)
+
+    def forward(self, x):
+        x = self.transblock1(self.conblock1(x))
+        x = self.transblock2(self.conblock2(x))
+        x = self.transblock3(self.conblock3(x))
+        x = self.conblock4(x)
+        x = self.gap(x)
+        x = x.view(-1, 10)
+
+        return F.log_softmax(input=x, dim=-1)
